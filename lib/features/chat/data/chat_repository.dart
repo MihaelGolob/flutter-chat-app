cc7b8c9fda6218f4c171850d7443eebf2e70ae08
mihael.golob <golob.mihael@gmail.com>
2024-08-03 11:02:57 +0200

    fix chat repository interface

--- lib/features/chat/data/chat_repository.dart
+++ lib/features/chat/data/chat_repository.dart
@@ -2,7 +2,9 @@ import 'package:chat_app/features/auth/models/user_model.dart';
 import 'package:chat_app/features/chat/models/message_model.dart';
 
 abstract class ChatRepository {
-  List<User> getAllUsers();
-  List<Message> getAllMessagesForUser(User user);
-  Message getLastMessageForUser(User user);
+  Future<List<User>> getAllUsers();
+  Future<List<Message>> getAllMessagesForUser(User user);
+  Future<Message> getLastMessageForUser(User user);
+
+  Future<void> sendMessage(User user, Message message);
 }
